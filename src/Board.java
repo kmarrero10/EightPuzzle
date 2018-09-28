d881c3ae7dd893e24c0ba50238f31b29d21e01f8
Kyle Marrero <kylemarrero@Kyles-MacBook-Pro.local>
2018-09-28 00:36:17 -0500

    pq

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,49 @@
 public class Board {
 
+    Board prev;
+
     int n;
 
     int[][] blocks;
 
     public Board(int[][] blocks) {
         this.blocks = blocks;
+    }
+
+//    private class Node {
+//
+//    }
+
+    public int dimension() {
+        return n;
+    }
+
+    public int hamming() {
+
+    }
+
+    public int manhattan() {
+
+    }
+
+    public boolean isGoal() {
+
+    }
+
+    public Board twin() {
+
+    }
+
+    public boolean equals(Object y) {
+
+    }
+
+    public Iterable<Board> neighbors() {
+
+    }
+
+    public String toString() {
+
     }
 
 }
--- src/Solver.java
+++ src/Solver.java
@@ -1,4 +1,26 @@
+import edu.princeton.cs.algs4.MinPQ;
+
 public class Solver {
+
+    public Solver(Board initial) {
+        MinPQ minPQ = new MinPQ(initial.n * initial.n);
+    }
+
+    public boolean isSolvable() {
+
+    }
+
+    public int moves() {
+
+        if (!isSolvable()) {
+            return -1;
+        }
+    }
+
+    public Iterable<Board> solution() {
+
+    }
+
     public static void main(String[] args) {
         int n=0;
         int[][] blocks = new int[n][n];
